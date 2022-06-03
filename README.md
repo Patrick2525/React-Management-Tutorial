df1b02332b0420a496b4efa9614a90c7b490fdc3
Patrick <knmswy2136@gmail.com>
2022-06-03 17:30:12 +0900

    Create Customer.js

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-## Management System
-본 프로젝트는 React 강의 목적으로 만들어진 고객 관리 시스템
+# Management System
+본 프로젝트는 React와 Node.js를 공부하기 위한 클론코딩 프로젝트로서 고객관리 시스템 
 (Management System)입니다.
 
 
--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Customer from './components/Customer';
+
+const customers = [{
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '홍길동', 
+  'birthday': '961222',
+  'gender': '남자',
+  'job': '대학생'
+},{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': '나상진', 
+  'birthday': '780322',
+  'gender': '남자',
+  'job': '프로그래머'
+},{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': '최의진', 
+  'birthday': '870625',
+  'gender': '남자',
+  'job': '의사'
+}]
 
 function App() { // App.js => 실질적으로 웹사이트의 화면에 대한 내용 출력을 담당하는 부분
   return (
-    <div className="gray-background">
-     <img src={logo} alt='logo'/>
-     <h2>Let's develop management system!</h2>
+    <div>
+      {
+        customers.map(c => {
+          return (
+            <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>
+          )
+        })
+      }
     </div>
   );
 }
